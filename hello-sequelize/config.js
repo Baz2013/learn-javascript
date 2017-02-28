1fd0b2c51a0d82ba78c3702edb8321961d9e5420
Baz2013 <chuan.gu6@gmail.com>
2017-03-01 02:33:24 +0800

    test node.js connect mysql

---
+++ hello-sequelize/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test', // 使用哪个数据库
+    username: 'www', // 用户名
+    password: 'www', // 口令
+    host: '123.57.14.82', // 主机名
+    port: 5306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
